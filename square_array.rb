30fb1a4ab62c3d3b52c7295c56d47662174226ff
Carl J <carlj1881@yahoo.com>
2020-09-14 22:25:51 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(numbers)
   numbers.each do |number|
-  numbers_new = number **
+  numbers_new = number * number
   puts "The number squared is #{numbers_new}"
   numbers_new
   end
