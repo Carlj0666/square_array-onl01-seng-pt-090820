787de33b293112d20e1e456d812d2c574632141b
Carl J <carlj1881@yahoo.com>
2020-09-14 22:38:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,9 +4,6 @@ def square_array(numbers)
   numbers_new = number * number
   numbers_array.push(numbers_new)
   puts "The number squared is #{numbers_new}"
-  
   end
   return numbers_array
 end
-# non return sees to be the issue, thelast line should return, but even given
-#the return command, between 6-7, this causes the code to break
